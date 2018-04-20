d647d1865e1bc15f4312b99cbfc71e02ee66a04f
= <noureldean.saed@gmail.com>
2018-04-20 22:39:19 +0300

    Remodified the navbar and content of the landing page

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { Error404Client } = require('./errorController');
 const landingPageController = require('./landingPageController');
-
+const shopProductController = require('./shopProductController');
 
 const Router = express.Router();
 
 Router.get('/', landingPageController);
+Router.get('/shop/:product', shopProductController);
 Router.use(Error404Client);
 
 module.exports = Router;
